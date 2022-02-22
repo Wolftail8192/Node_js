62b71fad7689c39dc21ad8cd42a0cf7ebce8354f
Wolftail8192 <4420299@gmail.com>
2022-02-22 13:15:43 +0200

    err F

---
+++ lesson4/dist/app.js
@@ -0,0 +1,11 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : {"default": mod};
+}
+exports.__esModule = true;
+var express_1 = __importDefault(require("express"));
+var app = express_1["default"]();
+app.listen(5500, function () {
+    console.log('Server has started ');
+});
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwLmpzIiwic291cmNlUm9vdCI6Ii9ob21lL3dvbGZ0YWlsL1dlYnN0b3JtUHJvamVjdHMvTm9kZV9qcy9sZXNzb240LyIsInNvdXJjZXMiOlsiYXBwLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsb0RBQThCO0FBRTlCLElBQU0sR0FBRyxHQUFHLG9CQUFPLEVBQUUsQ0FBQztBQUN0QixHQUFHLENBQUMsTUFBTSxDQUFDLElBQUksRUFBRTtJQUNiLE9BQU8sQ0FBQyxHQUFHLENBQUMscUJBQXFCLENBQUMsQ0FBQTtBQUN0QyxDQUFDLENBQUMsQ0FBQSIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCBleHByZXNzIGZyb20gJ2V4cHJlc3MnO1xuXG5jb25zdCBhcHAgPSBleHByZXNzKCk7XG5hcHAubGlzdGVuKDU1MDAsICgpID0+IHtcbiAgICBjb25zb2xlLmxvZygnU2VydmVyIGhhcyBzdGFydGVkICcpXG59KSJdfQ==
